19a3ab5780dd8a6f98586573d16545e6a8f5a651
kathomas921 <katharinethomas@Katharines-MacBook-Pro.local>
2014-10-26 21:50:03 -0500

    Initial Commit

---
+++ CSE425_Lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE425_Lab2
+//
+//  Created by Katharine Thomas on 10/26/14.
+//  Copyright (c) 2014 Katharine Thomas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
